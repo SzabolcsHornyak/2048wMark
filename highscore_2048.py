e68ff64904e2304ec93d273398fc7c9b63c57d99
borzfele <borzfele@gmail.com>
2017-03-29 16:44:11 +0200

    working highscore export function

--- highscore_2048.py
+++ highscore_2048.py
@@ -1,10 +1,26 @@
 # highscore
+import os.path
 def highscore():
-    open_file = open("highscore_2048.csv", "r")
-    highscore = open_file.read()
-    highscore_list = highscore.split(', ')
-    open_file.close()
-    
+    score = 666
+    w = 9
+    h = 3
+    filename = 'highscore_2048_{}x{}.csv'.format(w,h)
+    if os.path.isfile(filename):
+        open_file = open(filename, "r+")
+        hs = open_file.read()
+        highscore_list = hs.split(', ')
+        player_name = (str(input('Write here your name: ')))
+        open_file.write(', ' + str(player_name) + ': ' + str(score))
+    else:
+        open_file = open(filename, "w")
+        player_name = (str(input('Write here your name: ')))        
+        open_file.write(str(player_name) + ', ' + str(score))
+    open_file = open(filename, "r")
+    hs = open_file.read()
+    highscore_list = hs.split(', ')
+    highscore_dict = {}
+    for i in highscore_list:
+        i = highscore_dict[i]
+    print(highscore_dict)
 
-    print(highscore_list)
 highscore()
