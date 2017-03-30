6cf3619e25af0438e23f01d7802502565e532d9f
borzfele <borzfele@gmail.com>
2017-03-30 17:34:44 +0200

    működő hs import-export, print. geczymheee

--- highscore_2048.py
+++ highscore_2048.py
@@ -1,20 +1,13 @@
 # highscore
 import os.path
-def highscore():
-    w = h = 3
-    score = 666
+def highscore(player_name, score, w, h):
     filename = 'highscore_2048_{}x{}.csv'.format(w,h)
     if os.path.isfile(filename):
         open_file = open(filename, "r+")
         hs = open_file.read()
         highscore_list = hs.split(', ')
-        player_name = (str(input('Write here your name: ')))
         open_file.write(', ' + str(player_name) + ', ' + str(score))
     else:
-        open_file = open(filename, "w")
-        player_name = (str(input('Write here your name: ')))        
+        open_file = open(filename, "w")       
         open_file.write(str(player_name) + ', ' + str(score))
     open_file.close()
-    print(hs)
-
-highscore()
--- highscore_print_2048.py
+++ highscore_print_2048.py
@@ -4,21 +4,19 @@ az import_listből dictet csinálni
 rendezni a dictet value szerint
 kinyomtatni a dictet a többi fossal együtt
 '''
-
+from operator import itemgetter
 from highscore_2048 import *    
 def highscore_print():
 #imports data from .csv
-    #filename = 'highscore_2048_{}x{}.csv'.format(w,h)
-    open_file = open('highscore_2048_3x3.csv', "r")
+    filename = 'highscore_2048_{}x{}.csv'.format(w,h)
+    open_file = open('filename', "r")
     hs = list(open_file.read().split(', '))
     open_file.close()
-    #hs = {hs[i]: hs[i+1] for i in range(0, len(hs), 2)}
     hs_dict = {}
-    for i in hs:
-        hs_dict ={hs[i] : int(hs[i+1])}
-
-    print(hs)
-    '''length = 22
+    hs_dict = dict((hs[i], int(hs[i + 1])) for i in range(0, len(hs), 2))
+    hs_dict = list(sorted(tuple(hs_dict.items()), key=itemgetter(1)))
+    print(hs_dict)
+    length = 22
     h_score_first_row =  ' _     _       _                            '
     h_score_second_row = '| |   (_)     | |                           '
     h_score_third_row =  '| |__  _  __ _| |__  ___  ___ ___  _ __ ___ '
@@ -30,5 +28,4 @@ def highscore_print():
     print(h_score_first_row + '\n' + h_score_second_row + '\n' + h_score_third_row + '\n' + h_score_fourth_row + '\n' + h_score_fifth_row + '\n' + h_score_sixth_row + '\n' + h_score_seventh_row + '\n' + h_score_eigth_row + '\n\n' + 'player name'.ljust(length, ' ') + 'score'.rjust(length, ' ') + '\n' + '-'*length*2)
     for i in range(0, 11):
         if i % 2 == 1:
-            print('\n' + str(hs[i - 1]).ljust(length, ' ') + str(hs[i]).rjust(length, ' '))'''
-highscore_print()
+            print('\n' + str(hs[i - 1]).ljust(length, ' ') + str(hs[i]).rjust(length, ' '))
